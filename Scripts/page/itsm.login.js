deaa5799746c0cf408751760e3222859c84ca65f
IT PLANT <arrachmansandry@gmail.com>
2024-08-15 09:28:10 +0700

    push login enter

--- Scripts/page/itsm.login.js
+++ Scripts/page/itsm.login.js
@@ -40,3 +40,44 @@ function login() {
         }
     })
 }
+
+$("#txtPassword").keypress(function (event) {
+    if (event.keyCode == 13) {
+        const loading = document.querySelector('#modalOverlay');
+        const modal = bootstrap.Modal.getInstance(loading);
+        $('#modalOverlay').modal('show')
+
+
+        var S_DATA = {
+            username: $("#txtUsername").val(),
+            password: $("#txtPassword").val(),
+        }
+
+        $.ajax({
+            url: urlPath + 'api/user/login',
+            type: 'POST',
+            dataType: "json",
+            cache: false,
+            async: false,
+            data: JSON.stringify({ model: S_DATA }),
+            contentType: "application/json; charset=utf-8",
+            success: function (res) {
+                setTimeout(function () {
+                    console.log(res)
+                    if (res.StatusCode == 200) {
+                        alert(res.StatusMessage)
+                        window.location = urlPath + "login/role";
+                        $('#modalOverlay').modal('hide')
+                    }
+                    else {
+                        alert(res.StatusMessage)
+                        $('#modalOverlay').modal('hide')
+                    }
+                }, 3000)
+
+            }
+        })
+    } else {
+        
+    }
+})
